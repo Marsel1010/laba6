c5c54e4c6bccd368d992ec2072d1325a27c17284
Marsel1010 <zurabichmarcel@gmail.com>
2025-05-31 22:17:02 +0300

    Initial commit for lab3 project with CMake setup

---
+++ solver_lib/solver_lib.cpp
@@ -0,0 +1,8 @@
+// lab3/solver_lib/solver_lib.cpp
+#include "solver_lib.h"
+#include <iostream> // Для вывода в консоль, например
+
+void solve() {
+    std::cout << "Solving the problem using solver_lib functionality." << std::endl;
+    // Здесь могла бы быть сложная логика решения
+}
---
+++ solver_lib/solver_lib.h
@@ -0,0 +1,6 @@
+// lab3/solver_lib/solver_lib.h
+#pragma once
+#include <string> // Если нужны строки, или другие типы
+
+void solve(); // Объявление функции solve
+// Могут быть и другие функции, которые предоставляет solver_lib

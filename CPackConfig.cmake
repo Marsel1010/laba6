45726f5ba5715d87963bd48a648d124f481a5176
Marsel1010 <zurabichmarcel@gmail.com>
2025-06-09 23:29:45 +0300

    Подготовка к выпуску v1.0.6

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,22 +1,40 @@
-# ~/lab6/lab3-4/CPackConfig.cmake
-
+# Имя пакета
 set(CPACK_PACKAGE_NAME "solver")
+
+# Версия пакета
 set(CPACK_PACKAGE_VERSION "1.0.0")
+
+# Описание пакета
 set(CPACK_PACKAGE_DESCRIPTION "Solver Application from Lab Project")
+
+# Контакт разработчика
 set(CPACK_PACKAGE_CONTACT "your.email@example.com")
 
-# Для .deb
+# Лицензия (обязательно для RPM/DEB)
+set(CPACK_RPM_PACKAGE_LICENSE "MIT")
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+
+# Поддержка мультимедиа иконок (если нужно)
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-Linux")
+
+# Генераторы: TGZ, ZIP, DEB, RPM
+set(CPACK_GENERATOR "TGZ;ZIP;DEB;RPM")
+
+# Путь установки внутри пакета
+set(CPACK_PACKAGING_INSTALL_PREFIX "/usr")
+
+# Настройки для RPM
+set(CPACK_RPM_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-Linux.rpm")
+set(CPACK_RPM_PACKAGE_SUMMARY "Solver Application from Lab Project")
+set(CPACK_RPM_PACKAGE_GROUP "Applications/Engineering")
+set(CPACK_RPM_PACKAGE_RELEASE 1) # Номер релиза
+
+# Настройки для DEB
+set(CPACK_DEBIAN_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-Linux.deb")
+set(CPACK_DEBIAN_PACKAGE_SECTION "utils")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Your Name")
 
-# Генераторы
-set(CPACK_GENERATOR "TGZ;ZIP;DEB")
-
-# Настройки для source package
+# Исключаем ненужные файлы из source package
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
 set(CPACK_SOURCE_IGNORE_FILES "/build" "\\.git" "~$" "\\.swp$")
-
-# Дополнительно
-set(CPACK_MONOLITHIC_INSTALL TRUE)
-set(CPACK_PACKAGING_INSTALL_PREFIX "/")
-
-include(CPack)
